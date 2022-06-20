8d4d7711fa021c690f34da865e9c36db8358c137
Akash <akash.dakktm@gmail.com>
2022-06-20 20:46:56 +0530

    Added security

--- src/main/java/com/greatlearning/microservice/controller/AdminUserController.java
+++ src/main/java/com/greatlearning/microservice/controller/AdminUserController.java
@@ -8,12 +8,14 @@ import com.greatlearning.microservice.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
+@Secured("ADMIN")
 public class AdminUserController {
 
     @Autowired
